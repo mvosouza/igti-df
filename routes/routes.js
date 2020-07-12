98743249bc1ecab51afc151fc151f4de507954fe
Marcos Souza <marcos.oliversouza@gmail.com>
2020-07-12 14:11:53 -0300

    Added a error message on the default route

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const transactionRouter = express.Router();
 
+//Block the route without the period parameter
+transactionRouter.get('', (req, res) => {
+  res.json({
+    error:
+      'É necessário informar o parâmetro "period", cujo valor deve estar no formato yyyy-mm',
+  });
+});
+
 module.exports = transactionRouter;
